5a5ed847202f2de4a05ea966b2362d63dc734533
MoeBrowne <moebrowne@users.noreply.github.com>
2015-12-30 17:14:10 +0000

    Added a couple of skeletal classes

---
+++ channel.php
@@ -0,0 +1,33 @@
+<?php namespace YouTubeSubscription;
+
+class channel
+{
+
+    /**
+     * Initiate
+     */
+    public function __construct()
+    {
+    }
+
+    private function getHeaders()
+    {
+
+    }
+
+    public function download()
+    {
+
+    }
+
+    private function isCached()
+    {
+
+    }
+
+    private function isExpired()
+    {
+
+    }
+
+}
---
+++ subscriptions.class.php
@@ -0,0 +1,29 @@
+<?php namespace YouTubeSubscription;
+
+class subscriptions {
+
+    /**
+     * Initiate!
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function loadChannels(array $channelArray)
+    {
+
+    }
+
+    public function render()
+    {
+
+    }
+
+    private function getChannel($channelID)
+    {
+
+    }
+
+
+}

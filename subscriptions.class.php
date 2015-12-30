18a26ed69f5eb6917e822d81168ea05e0f0f513c
MoeBrowne <moebrowne@users.noreply.github.com>
2015-12-30 18:12:18 +0000

    Added wrapper methods for loading a set of channels

--- subscriptions.class.php
+++ subscriptions.class.php
@@ -1,29 +1,56 @@
 <?php namespace YouTubeSubscription;
 
+use DateTime;
+use SimpleXMLElement;
+
 class subscriptions {
+
+    public $channelIDs = [];
+    public $channels = [];
+    public $videos = [];
 
     /**
      * Initiate!
+     * @param array $channelArray
      */
-    public function __construct()
+    public function __construct(array $channelArray)
     {
-
+        $this->channelIDs = $channelArray;
+        $this->loadChannels();
     }
 
-    public function loadChannels(array $channelArray)
+    private function loadChannels()
     {
-
+        foreach ($this->channelIDs as $channelID) {
+            $this->channels[] = new channel($channelID);
+        }
     }
 
     public function render()
     {
+        foreach ($this->channels as $channelObj) {
+
+            $channel = new SimpleXMLElement($channelObj->XML);
+
+            foreach ($channel->entry as $video) {
+
+                $media = $video->children('http://search.yahoo.com/mrss/')->group;
+                $YTID = (string)$video->children('http://www.youtube.com/xml/schemas/2015')->videoId;
+
+                $timestamp = DateTime::createFromFormat('Y-m-d\TH:i:sP', (string)$video->published)->getTimestamp();
+
+                $this->videos[(int)$timestamp] = (object)[
+                    'title' => $video->title,
+                    'date' => (string)$video->published,
+                    'timestamp' => $timestamp,
+                    'URL' => 'https://www.youtube.com/embed/'.$YTID,
+                    'thumbnail' => (string)$media->thumbnail->attributes()->url,
+                ];
+            }
+        }
+
+        krsort($this->videos, SORT_NUMERIC);
 
     }
-
-    private function getChannel($channelID)
-    {
-
-    }
-
 
 }

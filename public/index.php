3c3b29551079ba509a509693b177d7d9b7e38dc1
MoeBrowne <moebrowne@users.noreply.github.com>
2025-03-22 19:21:40 +0000

    Remove black bars

--- public/index.php
+++ public/index.php
@@ -89,6 +89,7 @@ function e(?string $value): string {
             display: block;
             width: 100%;
             height: 100%;
+            object-fit: cover;
         }
 
         dialog {
